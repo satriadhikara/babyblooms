54a5f496e88b11c1e058310d48350887313e8b67
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-03-12 14:35:17 +0700

    feat(page): add landing page

--- ui/app/index.tsx
+++ ui/app/index.tsx
@@ -9,14 +9,15 @@ import {
   Platform,
   Dimensions,
 } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 import { ThemedText } from "@/components/ui/ThemedText";
+import GoogleIcon from "@/assets/vectors/google";
+import MetaIcon from "@/assets/vectors/meta";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <Image
-        source={require("@/assets/images/image.png")}
+        source={require("@/assets/images/landing-page-bg.png")}
         style={{
           width: Dimensions.get("window").width,
           height: Dimensions.get("window").height,
@@ -27,48 +28,78 @@ export default function App() {
         resizeMode="cover"
       />
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
-        <SafeAreaView
-          style={{
-            height: "50%",
-            paddingHorizontal: 24,
-          }}
-        >
-          <ThemedText
-            type="displaySmall"
-            style={{
-              color: "#992269",
-              marginTop: 77,
-              marginBottom: 16,
-            }}
-          >
-            Siap Memulai Perjalananmu?
-          </ThemedText>
-          <Text style={{ marginBottom: 48 }}>
-            Kehamilan penuh kejutan, dan kami siap menjadi teman setiamu!
-          </Text>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "#4285F4",
-              padding: 12,
-              borderRadius: 8,
-              marginBottom: 10,
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ color: "white" }}>Masuk dengan Google</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "#3b5998",
-              padding: 12,
-              borderRadius: 8,
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ color: "white" }}>Masuk dengan Facebook</Text>
-          </TouchableOpacity>
+        <SafeAreaView style={{ flex: 0 }}>
+          <View style={styles.container}>
+            <ThemedText
+              type="displaySmall"
+              style={{
+                color: "#992269",
+                marginBottom: 16,
+              }}
+            >
+              Siap Memulai Perjalananmu?
+            </ThemedText>
+            <Text style={{ marginBottom: 48 }}>
+              Kehamilan penuh kejutan, dan kami siap menjadi teman setiamu!
+            </Text>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "white",
+                padding: 12,
+                borderRadius: 32,
+                marginBottom: 10,
+                alignItems: "center",
+                flexDirection: "row",
+                justifyContent: "center",
+                gap: 10,
+              }}
+            >
+              <GoogleIcon />
+              <Text
+                style={{
+                  color: "#BA2980",
+                  fontFamily: "Switzer-Medium",
+                  fontWeight: "medium",
+                }}
+              >
+                Masuk dengan Google
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#4A4F87",
+                padding: 12,
+                borderRadius: 32,
+                alignItems: "center",
+                flexDirection: "row",
+                justifyContent: "center",
+                gap: 10,
+              }}
+            >
+              <MetaIcon />
+              <Text
+                style={{
+                  color: "white",
+                  fontFamily: "Switzer-Medium",
+                  fontWeight: "medium",
+                }}
+              >
+                Masuk dengan Meta
+              </Text>
+            </TouchableOpacity>
+          </View>
         </SafeAreaView>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 24,
+    paddingBottom: 24,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+});
