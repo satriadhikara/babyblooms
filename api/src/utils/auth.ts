9a5ac9443d41191eca6e264cd24c289534d23d57
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-03-13 14:08:01 +0700

    chore(init): Initialize auth base

---
+++ api/src/utils/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../db";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	socialProviders: {
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		},
+		facebook: {
+			clientId: process.env.FACEBOOK_CLIENT_ID as string,
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+		},
+	},
+});
