7d825f8793c5f3033085b2604a99eb900cbc8af3
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-03-18 22:47:51 +0700

    feat: auth api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,42 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { authRoute } from "./routes/auth";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { auth } from "./utils/auth";
 
-const app = new Hono()
+const app = new Hono<{
+	Variables: {
+		user: typeof auth.$Infer.Session.user | null;
+		session: typeof auth.$Infer.Session.session | null;
+	};
+}>().basePath("/api");
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger());
+app.use(
+	"*", // or replace with "*" to enable cors for all routes
+	cors({
+		origin: "exp://", // replace with your origin
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
+app.use("*", async (c, next) => {
+	const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
-export default app
+	if (!session) {
+		c.set("user", null);
+		c.set("session", null);
+		return next();
+	}
+
+	c.set("user", session.user);
+	c.set("session", session.session);
+	return next();
+});
+
+app.route("/auth", authRoute);
+
+export default app;
--- api/src/utils/auth.ts
+++ api/src/utils/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
+import { expo } from "@better-auth/expo";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -11,9 +12,11 @@ export const auth = betterAuth({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		},
-		facebook: {
-			clientId: process.env.FACEBOOK_CLIENT_ID as string,
-			clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-		},
+		// facebook: {
+		// 	clientId: process.env.FACEBOOK_CLIENT_ID as string,
+		// 	clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+		// },
 	},
+	plugins: [expo()],
+	trustedOrigins: ["exp://"],
 });
