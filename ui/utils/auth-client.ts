40584695c9b2fcc08fff7ffc76cdc7b0ad39dc05
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-03-20 23:58:00 +0700

    fix: localhost endpoint on android

--- ui/utils/auth-client.ts
+++ ui/utils/auth-client.ts
@@ -1,10 +1,16 @@
 import { createAuthClient } from "better-auth/react";
 import { expoClient } from "@better-auth/expo/client";
 import * as SecureStore from "expo-secure-store";
-import { customSessionClient } from "better-auth/client/plugins";
+import { Platform } from "react-native";
 
+const getBaseUrl = () => {
+	if (Platform.OS === "android") {
+		return "http://10.0.2.2:3000";
+	}
+	return "http://localhost:3000";
+};
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:3000",
+	baseURL: getBaseUrl(),
 	plugins: [
 		expoClient({
 			scheme: "babyblooms",
