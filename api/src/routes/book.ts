4ebe7cc5d1eb337452c1d4b989df5cd2160a4508
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-04-12 08:58:44 +0700

    feat: add recent query

--- api/src/routes/book.ts
+++ api/src/routes/book.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { auth } from "../utils/auth";
 import { dailyBook, guardian, user as userTable } from "../db/schema";
 import { db } from "../db";
-import { eq } from "drizzle-orm";
+import { desc, eq, SQL } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import { randomUUIDv7 } from "bun";
@@ -14,47 +14,62 @@ export const bookRoute = new Hono<{
   };
 }>();
 
-bookRoute.get("/", async (c) => {
-  const session = c.get("session");
-  const user = c.get("user");
+bookRoute.get(
+  "/",
+  zValidator(
+    "query",
+    z.object({
+      recentOnly: z.string().optional(),
+    })
+  ),
+  async (c) => {
+    const session = c.get("session");
+    const user = c.get("user");
+    const validated = c.req.valid("query");
+    const recentOnly = validated.recentOnly === "true";
 
-  if (!session || !user) {
-    return c.json({ error: "Unauthorized" }, 401);
-  }
-
-  try {
-    let motherId = user.id;
-    // Check roles
-    if (user.role !== "mom") {
-      const [resp] = await db
-        .select()
-        .from(guardian)
-        .where(eq(guardian.guardianUserId, user.id));
-      if (!resp) {
-        return c.json({ error: "Guardian not found" }, 404);
-      }
-      motherId = resp.motherId;
+    if (!session || !user) {
+      return c.json({ error: "Unauthorized" }, 401);
     }
 
-    const books = await db
-      .select({
-        id: dailyBook.id,
-        content: dailyBook.content,
-        createdAt: dailyBook.createdAt,
-        userId: userTable.id,
-        name: userTable.name,
-        imageUrl: userTable.image,
-      })
-      .from(dailyBook)
-      .fullJoin(userTable, eq(dailyBook.userId, userTable.id))
-      .where(eq(dailyBook.motherId, motherId));
+    try {
+      let motherId = user.id;
+      // Check roles
+      if (user.role !== "mom") {
+        const [resp] = await db
+          .select()
+          .from(guardian)
+          .where(eq(guardian.guardianUserId, user.id));
+        if (!resp) {
+          return c.json({ error: "Guardian not found" }, 404);
+        }
+        motherId = resp.motherId;
+      }
 
-    return c.json(books, 200);
-  } catch (error) {
-    console.error("Error fetching books:", error);
-    return c.json({ error: "Failed to fetch books" }, 500);
+      const books = await db
+        .select({
+          id: dailyBook.id,
+          content: dailyBook.content,
+          createdAt: dailyBook.createdAt,
+          userId: userTable.id,
+          name: userTable.name,
+          imageUrl: userTable.image,
+        })
+        .from(dailyBook)
+        .fullJoin(userTable, eq(dailyBook.userId, userTable.id))
+        .where(eq(dailyBook.motherId, motherId))
+        .orderBy(desc(dailyBook.createdAt));
+
+      if (recentOnly) {
+        return c.json(books[0], 200);
+      }
+      return c.json(books, 200);
+    } catch (error) {
+      console.error("Error fetching books:", error);
+      return c.json({ error: "Failed to fetch books" }, 500);
+    }
   }
-});
+);
 
 bookRoute.post(
   "/",
@@ -110,3 +125,6 @@ bookRoute.post(
     }
   }
 );
+function orderBy(arg0: SQL<unknown>) {
+  throw new Error("Function not implemented.");
+}
